4f65135b3986279aa1f97fda085b5fcf5736305a
holotester <110076171+holotester@users.noreply.github.com>
2023-11-10 12:34:35 +0800

    Update Readxml.cs

--- Assets/Scripts/Readxml.cs
+++ Assets/Scripts/Readxml.cs
@@ -142,7 +142,7 @@ public class Readxml : MonoBehaviour
         pop = GetComponent<PopupManager>();
         animator = GetComponent<Animator>();
         gc = GetComponent<GC>();
-        StartCoroutine(ReadXML("https://raw.githubusercontent.com/20145050-Vernon-Ong/Active-Mobility-Game/main/Xml/popupNotifi.xml"));
+        StartCoroutine(ReadXML("https://raw.githubusercontent.com/holotester/LTA-Active-Mobility-Act/master/Xml/popupNotifi.xml"));
     }
     
 
